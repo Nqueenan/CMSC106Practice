e9bb506f0f81f7359b8307e60210a4c484d6e226
Natalie Queenan <nqueenan@haverford.edu>
2026-02-10 10:50:58 -0500

    yay

---
+++ Linked_Lists/Node.java
@@ -0,0 +1,17 @@
+public class Node<E> 
+{
+    private E element ;
+    private Node<E> next ;
+
+    public Node(E element, Node<E> next)
+    {
+        this.element = element ;
+        this.next = null ;
+    }
+
+    // add getters and setters
+    public static void main(String[] args) 
+    {
+        
+    }
+}
